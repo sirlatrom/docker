c991ca92171b5e61f860d5b1f2b5cd1b4368c8b9
Sune Keller <sune.keller@gmail.com>
2018-12-27 23:18:38 +0100

    Add secret and config references to PluginSpec
    Signed-off-by: Sune Keller <sune.keller@gmail.com>

--- api/types/swarm/runtime/gen.go
+++ api/types/swarm/runtime/gen.go
@@ -1,3 +1,3 @@
-//go:generate protoc -I . --gogofast_out=import_path=github.com/docker/docker/api/types/swarm/runtime:. plugin.proto
+//go:generate protoc -I . -I $GOPATH/src -I ../../../../vendor/github.com/gogo/protobuf/ --gogofast_out=import_path=github.com/docker/docker/api/types/swarm/runtime:. plugin.proto
 
 package runtime // import "github.com/docker/docker/api/types/swarm/runtime"
